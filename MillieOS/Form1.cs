ddfcea3f39890d07f48072d2f09cae41a06f1e58
brockstar299 <150369458+brockstar299@users.noreply.github.com>
2024-11-08 07:29:21 -0500

    Shutdown Update

--- MillieOS/Form1.cs
+++ MillieOS/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MillieOS
@@ -17,35 +11,77 @@ namespace MillieOS
             InitializeComponent();
         }
 
+        // Event handler for shutting down the OS
         private void shutdownToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            int confirmationCount = 0;  // Start with 0 "very"s
+
+            while (true)
+            {
+                // If it's the first time (confirmationCount == 0), show the base message without "very"
+                string message;
+                if (confirmationCount == 0)
+                {
+                    message = "Are you sure you want to shut down MillieOS?";
+                }
+                else
+                {
+                    // For subsequent times, add "very"s
+                    message = "Are you " + string.Join(" ", Enumerable.Repeat("very", confirmationCount)) + " sure you want to shut down MillieOS?";
+                }
+
+                // Show the message box and ask for confirmation
+                var result = MessageBox.Show(message, "Shutdown", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    // If the user confirms, increment the confirmation count
+                    confirmationCount++;
+
+                    // If the count exceeds a certain number (e.g., 5), shut down MillieOS
+                    if (confirmationCount >= 5)
+                    {
+                        Application.Exit();  // Shut down the app
+                        break;  // Exit the loop
+                    }
+                }
+                else
+                {
+                    // If the user clicks "No", cancel the shutdown and exit the loop
+                    break;
+                }
+            }
         }
 
+        // Event handler to open the Cat App from the menu
         private void mToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Create an instance of CatForm
-            CatForm catForm = new CatForm();
-
-            // Show the CatForm
-            catForm.Show();  // Show the form non-modally
+            OpenCatForm();
         }
 
+        // Event handler to open the Cat App from button1
         private void button1_Click(object sender, EventArgs e)
         {
-            // Create an instance of CatForm
-            CatForm catForm = new CatForm();
+            OpenCatForm();
+        }
 
-            // Show the CatForm
+        // Event handler to open the File App from button2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileForm();
+        }
+
+        // Method to open the Cat App (Pet Millie)
+        private void OpenCatForm()
+        {
+            CatForm catForm = new CatForm();
             catForm.Show();  // Show the form non-modally
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        // Method to open the File Explorer (still in Beta)
+        private void OpenFileForm()
         {
-            // Create an instance of CatForm
             FileForm fileForm = new FileForm();
-
-            // Show the CatForm
             fileForm.Show();  // Show the form non-modally
         }
     }
